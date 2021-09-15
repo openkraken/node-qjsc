7b5ee71aab14b72c5b2af4ff0f62605e4afb9387
元彦 <yuanyan.cao@gmail.com>
2021-09-15 13:27:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const supportedVersion = ['20210327'];
+const supportedVersions = ['20210327'];
 
 class Qjsc {
-  constructor(version = '20210327') {
-    if (supportedVersion.indexOf(version) === -1) {
-      throw new Error('Unsupported quickjs version: ' + version);
+  constructor(options = {}) {
+    let version = options.version || '20210327';
+    if (supportedVersions.indexOf(version) === -1) {
+      throw new Error('Unsupported QuickJS version: ' + version);
     }
     this._bindings = require('bindings')(`qjsc_${version}.node`);
   }
   help() {
-    console.log('supported version: ' + supportedVersion.join(', '));
+    console.log('Supported QuckJS versions: ' + supportedVersions.join(', '));
   }
 
-  getAllSupportedVersion() {
-    return supportedVersion;
+  getSupportedVersions() {
+    return supportedVersions;
   }
 
-  dumpByteCode(code, sourceURL = 'internal://') {
+  compile(code, sourceURL = 'internal://') {
     return this._bindings.dumpByteCode(code, sourceURL);
   }
 
@@ -23,7 +24,7 @@ class Qjsc {
     return this._bindings.version;
   }
 
-  evalByteCode(buffer) {
+  _evalByteCode(buffer) {
     return this._bindings.evalByteCode(buffer);
   }
 }
